d947e7c09651f6f1bc68e70ac425f5fb47ab5397
PP2-21B030774 <azat.amenov@gmail.com>
2023-04-29 01:38:24 +0600

    SolTube 29.04 2

--- front-side/src/app/sign-in/sign-in.component.ts
+++ front-side/src/app/sign-in/sign-in.component.ts
@@ -12,7 +12,24 @@ export class SignInComponent {
   constructor(private location: Location) { }
 
   signIn(value: any, e: any) {
-    console.log(value.firstname)
+    const signIn = e.composedPath()[0] as HTMLElement
+    const error = document.querySelector(".error") as HTMLElement
+    this.checkToErrors(value, signIn, error)
+  }
+  checkToErrors(value: any, signIn: HTMLElement, error: HTMLElement) {
+    error.classList.add("show")
+    if(value.username == "" || value.password == "") {
+      error.innerHTML = "Fill in all the required fields !"
+      signIn.querySelectorAll(".input_block").forEach(inputBlock => {
+        if(inputBlock.querySelector("input").value == "") {
+          inputBlock.classList.add("fill_all")
+          setTimeout(() => {
+            inputBlock.classList.remove("fill_all")
+            error.classList.remove("show")
+          }, 3000)
+        }
+      })
+    }
   }
 
   showHidePassword(action: boolean, e: any) {
--- front-side/src/app/sign-up/sign-up.component.ts
+++ front-side/src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 
 import { Location } from '@angular/common'
 
@@ -9,16 +9,56 @@ import { Location } from '@angular/common'
 })
 export class SignUpComponent {
 
+  username = "azikkw"
+
   constructor(private location: Location) { }
 
   signUp(value: any, e: any) {
-    const signUp = e.composedPath()[0]
-
+    const signUp = e.composedPath()[0] as HTMLElement
+    const error = document.querySelector(".error") as HTMLElement
+    this.checkToErrors(value, signUp, error)
+  }
+  checkToErrors(value: any, signUp: HTMLElement, error: HTMLElement) {
+    error.classList.add("show")
     if(value.firstname == "" || value.lastname == "" || value.username == "" || value.password == "" || value.passwordVerify == "") {
-
+      error.innerHTML = "Fill in all the required fields !"
+      signUp.querySelectorAll(".input_block").forEach(inputBlock => {
+        if(inputBlock.querySelector("input").value == "") {
+          inputBlock.classList.add("fill_all")
+          setTimeout(() => {
+            inputBlock.classList.remove("fill_all")
+            error.classList.remove("show")
+          }, 3000)
+        }
+      })
+    } else if(value.password.length < 8 || value.passwordVerify < 8) {
+      error.innerHTML = "The password must be at least 8 characters long !"
+      signUp.querySelectorAll(".input_block.password").forEach(inputBlock => {
+        inputBlock.querySelector("input").value = ""
+        inputBlock.classList.add("fill_all")
+        setTimeout(() => {
+          inputBlock.classList.remove("fill_all")
+          error.classList.remove("show")
+        }, 3000)
+      })
+    } else if(value.password != value.passwordVerify) {
+      error.innerHTML = "Confirm the password to complete the registration !"
+      signUp.querySelectorAll(".input_block.password").forEach(inputBlock => {
+        inputBlock.querySelector("input").value = ""
+        inputBlock.classList.add("fill_all")
+        setTimeout(() => {
+          inputBlock.classList.remove("fill_all")
+          error.classList.remove("show")
+        }, 3000)
+      })
     }
-    if(value.password == "" || value.passwordVerify == "") {
-
+  }
+  existUsername() {
+    const usernameInput = document.querySelector(".username_input") as HTMLInputElement
+    if(usernameInput.value == this.username) {
+      document.querySelector(".username_exist").classList.add("show")
+    } else {
+      document.querySelector(".username_exist").classList.remove("show")
     }
   }
 
