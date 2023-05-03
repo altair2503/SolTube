58aed612fee6bf17e295cc845773447f24532c73
PP2-21B030774 <azat.amenov@gmail.com>
2023-05-03 18:50:01 +0600

    SolTube 03.05 2

--- front-side/src/app/video-upload/video-upload.component.ts
+++ front-side/src/app/video-upload/video-upload.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Location} from "@angular/common";
+import {JwtService} from "../jwt.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-video-upload',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./video-upload.component.css']
 })
 export class VideoUploadComponent {
+
+  constructor(private location: Location, private router: Router) { }
+
+  videoUpload(value: any, e: any) {
+
+  }
+
+  chooseNav(e: any) {
+    document.querySelectorAll(".navigation button").forEach(p => {
+      p.classList.remove("active")
+    })
+    e.composedPath()[0].classList.add("active")
+    console.log(e.composedPath()[0].dataset["nav"])
+    document.querySelector(".pages_container").className = `pages_container ${e.composedPath()[0].dataset["nav"]}`
+  }
+
+  returnBack() {
+    this.location.back()
+  }
 
 }
