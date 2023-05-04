df66e71f70ee9845db4f2de10b1f152d96efb5dd
PP2-21B030774 <azat.amenov@gmail.com>
2023-05-04 14:32:03 +0600

    SolTube 04.05 1

--- front-side/src/app/video-upload/video-upload.component.ts
+++ front-side/src/app/video-upload/video-upload.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 import {Location} from "@angular/common";
 import {JwtService} from "../jwt.service";
 import {Router} from "@angular/router";
@@ -8,9 +8,13 @@ import {Router} from "@angular/router";
   templateUrl: './video-upload.component.html',
   styleUrls: ['./video-upload.component.css']
 })
-export class VideoUploadComponent {
+export class VideoUploadComponent implements AfterViewInit {
 
   constructor(private location: Location, private router: Router) { }
+
+  ngAfterViewInit() {
+    this.openChooseCategory()
+  }
 
   videoUpload(value: any, e: any) {
 
@@ -23,6 +27,19 @@ export class VideoUploadComponent {
     e.composedPath()[0].classList.add("active")
     console.log(e.composedPath()[0].dataset["nav"])
     document.querySelector(".pages_container").className = `pages_container ${e.composedPath()[0].dataset["nav"]}`
+  }
+
+  openChooseCategory() {
+    document.addEventListener("click", e => {
+      let target = e.target as Element // @ts-ignore
+      if(target.className !== "input_block choose") {
+        document.querySelector(".select_category").classList.remove("open")
+      } else document.querySelector(".select_category").classList.add("open")
+    })
+  }
+  selectCategory(e: any) {
+    const categoryValue = document.querySelector(".input_block.choose input") as HTMLInputElement
+    categoryValue.value = e.composedPath()[0].innerHTML
   }
 
   returnBack() {
